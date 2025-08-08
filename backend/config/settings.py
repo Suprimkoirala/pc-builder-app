bc4d7eb210d2cf95f7a9bcede18aa531121f3e5a
Suprimkoirala <suprim.079bei043@tcioe.edu.np>
2025-08-08 09:56:10 +0545

    minor updates for postgres setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,16 +95,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME':     'dbms_project',
-        'USER':     'dbuser',
-        'PASSWORD': 'root',
-        'HOST':     '127.0.0.1',
-        'PORT':     '3306',
-        'OPTIONS': {
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbms_project',      # database name you just created
+        'USER': 'postgres',          # PostgreSQL user
+        'PASSWORD': '123',           # password you enter in pgAdmin
+        'HOST': 'localhost',         # or '127.0.0.1'
+        'PORT': '5432',               # PostgreSQL default port
     }
 }
 

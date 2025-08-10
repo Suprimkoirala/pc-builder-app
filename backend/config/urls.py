dbf00827f4ffec5d1fa80b0020d32ba4bc264c61
Suprimkoirala <suprim.079bei043@tcioe.edu.np>
2025-08-10 11:53:07 +0545

    added vercel.json and implemented healthz endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import (
     TokenBlacklistView,
     TokenRefreshView,
@@ -11,4 +12,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/logout/', TokenBlacklistView.as_view(), name='token_blacklist'), 
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('healthz/', lambda request: JsonResponse({"status": "ok"})),
 ]
